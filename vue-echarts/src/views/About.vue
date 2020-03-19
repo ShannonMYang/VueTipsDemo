2929e9cbb2f4211cd4b6fc1d765d35e676ad9fc1
Yang Xiukun <myang.shannon1122@gmail.com>
2020-03-19 18:13:53 +0800

    首次提交，包含了已经完成的折线图示例

---
+++ vue-echarts/src/views/About.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-03-19 15:51:34
+ * @LastEditTime: 2020-03-19 16:57:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-echarts\src\views\About.vue
+ -->
+<template>
+  <div class="line-div">
+    <h1>This is an LineChart page!!!</h1>
+    <div class="line-chart">
+      <LineChart></LineChart>
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart.vue';
+export default {
+  name: 'about',
+  components: {
+    LineChart
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .line-div {
+        height: 500px;
+        margin-top: 15px;
+        padding: 10px 10px 10px 10px;
+        background-color: #fff;
+        /*边框线宽度*/
+        border: 1px solid #ccc;
+        /*圆角化*/
+        border-radius: 5px;
+        /*阴影效果*/
+        box-shadow: 0 0 4px 0 #ccc;
+    }
+
+    .line-chart {
+        height: 300px;
+        margin-top: 15px;
+        padding: 10px 10px 10px 10px;
+        background-color: #fff;
+        /*边框线宽度*/
+        border: 1px solid #ccc;
+        /*圆角化*/
+        border-radius: 5px;
+        /*阴影效果*/
+        box-shadow: 0 0 4px 0 #ccc;
+    }
+
+    .line-chart:hover {
+        height: 300px;
+        margin-top: 15px;
+        padding: 10px 10px 10px 10px;
+        background-color: #fff;
+        /*边框线宽度*/
+        border: 1px solid #ccc;
+        /*圆角化*/
+        border-radius: 5px;
+        /*阴影效果*/
+        box-shadow: 0 0 8px 0 rgb(173, 69, 69);
+    }
+</style>
